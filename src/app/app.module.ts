e9cfd0fa783b32b847430ccb8c48e670ef4ac678
Marcin Czerwinski <marbroczerwinski@gmail.com>
2022-01-04 23:35:21 +0100

    add bb-ui module and set preprocessored styles for direct acces to variables

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BbUIModule } from '../bb-ui/bb-ui.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BbUIModule
   ],
   providers: [],
   bootstrap: [AppComponent]
